605e87414748dc3045077f3686ec81d4e5b3774e
Matan <matan.modai@gmail.com>
2017-09-09 15:58:21 +0300

    Change Campground model to have comments in it

--- models/campground.js
+++ models/campground.js
@@ -4,9 +4,13 @@ const mongoose = require('mongoose');
 var campgroundSchema = new mongoose.Schema({
   name: String, 
   image: String,
-  description: String
+  description: String,
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }]
 });
 
 var Campground = mongoose.model('Campground', campgroundSchema);
 
-module.exports = {Campground};
+module.exports = Campground;
